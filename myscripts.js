6870c57f8e3f14867629619deb30e5cd66593c8b
Aturnip <kubanajki@wp.pl>
2023-02-10 15:42:21 +0100

    pierwsza wersja z UI

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 const choices = ["rock", "paper", "scissors"];
 
 function getCompChoice() {
@@ -13,22 +11,48 @@ function playRound(computer_choice, player_choice) {
     else return player_choice === "rock" ? "Player wins" : "Computer wins";
 }
 
-function game() {
-    let typo = "";
-    for (i=1; i<6; i++) {
-        let person = prompt(typo + "Enter ur choice: ROCK/PAPER/SCISSORS");
-        typo = "";
-        if (person === null) {
-            console.log("Game aborted");
-            break;
-        }
-        else if (choices.includes(person.toLowerCase())) {
-            console.log(playRound(getCompChoice(), person.toLowerCase()));
-        }
-       
-        else {
-            typo = "You had a typo! ";
-            i--;
-        }
+function gameDemo(weapon) {
+    let result = playRound(getCompChoice(), weapon);
+    resultWindow.textContent = result;
+    if (result === "Player wins") {
+        playerPoints++;
+    } else if (result === "Computer wins") {
+        computerPoints++;
     }
+    computerPoints>=5 ? scoreComp.textContent = "Computer wins the match!" : scoreComp.textContent = "Computer: " + computerPoints;
+    playerPoints>=5 ? scorePlayer.textContent = "Player wins the match!" : scorePlayer.textContent = "Player: " + playerPoints;
 }
+
+// function game() {
+//     let typo = "";
+//     for (i=1; i<6; i++) {
+//         let person = prompt(typo + "Enter ur choice: ROCK/PAPER/SCISSORS");
+//         typo = "";
+//         if (person === null) {
+//             console.log("Game aborted");
+//             break;
+//         }
+//         else if (choices.includes(person.toLowerCase())) {
+//             console.log(playRound(getCompChoice(), person.toLowerCase()));
+//         }
+       
+//         else {
+//             typo = "You had a typo! ";
+//             i--;
+//         }
+//     }
+// }
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach( (button) => {
+    button.addEventListener("click", () => gameDemo(button.id));
+})
+
+let playerPoints = 0;
+let computerPoints = 0;
+const resultWindow = document.querySelector("#result");
+const scorePlayer = document.querySelector("#scorePlayer");
+const scoreComp = document.querySelector("#scoreComp");
+
+
+
